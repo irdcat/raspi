e69be9d6191cb96697b1acd4961985d9b2a034a0
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-10-20 13:50:37 +0200

    Modularize application #9

--- server/src/test/kotlin/irdcat/fitness/FitnessApplicationTests.kt
+++ server/src/test/kotlin/irdcat/fitness/FitnessApplicationTests.kt
@@ -1,8 +1,6 @@
 package irdcat.fitness
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 
 class FitnessApplicationTests : AbstractIntegrationTest() {
 
