8c6012e90c55bf757656e8f7c6b04ca470a5baca
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-10-24 21:36:41 +0200

    Test exercise data and exercise list page adjustments (#24)

--- server/src/test/kotlin/irdcat/fitness/LocalApplicationInitializer.kt
+++ server/src/test/kotlin/irdcat/fitness/LocalApplicationInitializer.kt
@@ -1,10 +1,14 @@
 package irdcat.fitness
 
-import irdcat.fitness.model.ExerciseDto
+import irdcat.fitness.model.Exercise
 import irdcat.fitness.repository.ExerciseRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.yaml.snakeyaml.Yaml
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Profile("!test")
 @Configuration
@@ -12,15 +16,27 @@ class LocalApplicationInitializer(
     private val exerciseRepository: ExerciseRepository
 ): InitializingBean {
 
+    companion object {
+        @JvmStatic
+        private val logger = LoggerFactory.getLogger(LocalApplicationInitializer::class.java)
+
+        private const val TEST_EXERCISES_FILE = "test-exercises.yaml"
+    }
+
     override fun afterPropertiesSet() {
-        exerciseRepository.insert(listOf(
-            ExerciseDto(name = "Bench Press", isBodyWeight = false).toExercise(),
-            ExerciseDto(name = "Barbell Row", isBodyWeight = false).toExercise(),
-            ExerciseDto(name = "Deadlift", isBodyWeight = false).toExercise(),
-            ExerciseDto(name = "Squat", isBodyWeight = false).toExercise(),
-            ExerciseDto(name = "Over Head Press", isBodyWeight = false).toExercise(),
-            ExerciseDto(name = "Dip", isBodyWeight = true).toExercise(),
-            ExerciseDto(name = "Pull Up", isBodyWeight = true).toExercise()
-        )).blockLast()
+        Mono.just(TEST_EXERCISES_FILE)
+            .map { this.javaClass.classLoader.getResourceAsStream(it) }
+            .map { Yaml().load<List<LinkedHashMap<String, Any>>>(it) }
+            .flatMapMany { Flux.fromIterable(it!!) }
+            .map {
+                Exercise(
+                    it["id"]!!.toString(),
+                    it["name"]!!.toString(),
+                    it["isBodyWeight"]!!.toString().toBoolean()
+                )
+            }
+            .collectList()
+            .flatMapMany { exerciseRepository.insert(it) }
+            .subscribe { logger.info("Added {} into database", it) }
     }
 }
