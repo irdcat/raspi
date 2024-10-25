222ebcc6c795009e263a49647b80db9c1822943a
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-10-25 14:01:51 +0200

    More trainings data and initialization in local setup (#27)

--- server/src/test/kotlin/irdcat/fitness/LocalApplicationInitializer.kt
+++ server/src/test/kotlin/irdcat/fitness/LocalApplicationInitializer.kt
@@ -1,7 +1,11 @@
 package irdcat.fitness
 
 import irdcat.fitness.model.Exercise
+import irdcat.fitness.model.Training
+import irdcat.fitness.model.TrainingExercise
+import irdcat.fitness.model.TrainingExerciseSet
 import irdcat.fitness.repository.ExerciseRepository
+import irdcat.fitness.repository.TrainingRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Configuration
@@ -9,24 +13,38 @@ import org.springframework.context.annotation.Profile
 import org.yaml.snakeyaml.Yaml
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 @Profile("!test")
 @Configuration
 class LocalApplicationInitializer(
-    private val exerciseRepository: ExerciseRepository
+    private val exerciseRepository: ExerciseRepository,
+    private val trainingRepository: TrainingRepository
 ): InitializingBean {
 
     companion object {
         @JvmStatic
         private val logger = LoggerFactory.getLogger(LocalApplicationInitializer::class.java)
 
+        @JvmStatic
+        private val yaml = Yaml()
+
+        @JvmStatic
+        private val dateTimeFormatter = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd")
+            .withZone(ZoneId.ofOffset("UTC", ZoneOffset.UTC))
+
         private const val TEST_EXERCISES_FILE = "test-exercises.yaml"
+        private const val TEST_TRAININGS_FILE = "test-trainings.yaml"
     }
 
     override fun afterPropertiesSet() {
         Mono.just(TEST_EXERCISES_FILE)
             .map { this.javaClass.classLoader.getResourceAsStream(it) }
-            .map { Yaml().load<List<LinkedHashMap<String, Any>>>(it) }
+            .map { yaml.load<List<LinkedHashMap<String, Any>>>(it) }
             .flatMapMany { Flux.fromIterable(it!!) }
             .map {
                 Exercise(
@@ -37,6 +55,35 @@ class LocalApplicationInitializer(
             }
             .collectList()
             .flatMapMany { exerciseRepository.insert(it) }
-            .subscribe { logger.info("Added {} into database", it) }
+            .subscribe { logger.info("Added exercise {}", it) }
+
+        Mono.just(TEST_TRAININGS_FILE)
+            .map { this.javaClass.classLoader.getResourceAsStream(it) }
+            .map { yaml.load<List<LinkedHashMap<String, Any>>>(it) }
+            .flatMapMany { Flux.fromIterable(it) }
+            .map {
+                Training(
+                    it["id"]!!.toString(),
+                    LocalDate.parse(it["date"]!!.toString(), dateTimeFormatter),
+                    it["bodyWeight"]!!.toString().toFloat(),
+                    (it["exercises"] as List<LinkedHashMap<String, Any>>)
+                        .map { e ->
+                            TrainingExercise(
+                                e["order"].toString().toInt(),
+                                e["exerciseId"].toString(),
+                                (e["sets"] as List<LinkedHashMap<String, Any>>)
+                                    .map { s ->
+                                        TrainingExerciseSet(
+                                            s["reps"]!!.toString().toInt(),
+                                            s["weight"]!!.toString().toFloat()
+                                        )
+                                    }
+                            )
+                        }
+                )
+            }
+            .collectList()
+            .flatMapMany { trainingRepository.insert(it) }
+            .subscribe { logger.info("Added training {}", it) }
     }
 }
