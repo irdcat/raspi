87ffcb6c4f4df93e02145c495fe8a5935e0c299c
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-10-29 23:20:32 +0100

    Handle exercise chart rendering in a separate component #39

---
+++ fitness-ui/src/components/ExerciseChart.tsx
@@ -0,0 +1,97 @@
+import { EChartsOption, SeriesOption } from "echarts"
+import { ExerciseParameters } from "../types"
+import EChartsReact from "echarts-for-react"
+
+type ExerciseChartProps = {
+    parameters: Map<Date, ExerciseParameters>,
+    height?: string | number,
+}
+
+const ExerciseChart = (props: ExerciseChartProps) => {
+
+    const paramsToData = (params: ExerciseParameters[]): { [key: string]: number[] } => {
+        const data: { [key: string]: number[] } = {};
+        params.forEach((param) => {
+            Object.keys(param).forEach(k => {
+                if (data[k] === undefined) {
+                    data[k] = [param[k as keyof ExerciseParameters]]
+                } else {
+                    data[k].push(param[k as keyof ExerciseParameters])
+                }
+            })
+        })
+        return data;
+    }
+
+    const formatFieldName = (name: string): string => {
+        const result = name.replace(/([A-Z])/g, ' $1');
+        return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
+    const dataToSeries = (data: { [key: string]: number[] }): SeriesOption[] => {
+        const series: SeriesOption[] = [];
+        for (const [key, value] of Object.entries(data)) {
+            const formattedName = formatFieldName(key);
+            series.push({
+                name: formattedName,
+                type: 'line',
+                data: value,
+            })
+        }
+        return series;
+    }
+
+    const dataToLegendData = (data: { [key: string]: number[] }): string[] => {
+        const legendData: string[] = [];
+        for (const key of Object.keys(data)) {
+            const name = formatFieldName(key);
+            legendData.push(name)
+        }
+        return legendData;
+    }
+
+    const parametersToEChartsOption = (params: Map<Date, ExerciseParameters>): EChartsOption => {
+        const paramData = paramsToData(Object.values(params));
+        return {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross'
+                }
+            },
+            legend: {
+                data: dataToLegendData(paramData),
+                top: '20'
+            },
+            xAxis: {
+                data: Object.keys(params),
+                axisTick: {
+                    alignWithLabel: true
+                }
+            },
+            yAxis: {
+                type: 'value',
+                alignTicks: true,
+                axisLabel: {
+                    formatter: "{value} kg"
+                },
+                axisLine: {
+                    show: true,
+                    lineStyle: {
+                        color: '#335656'
+                    }
+                }
+            },
+            series: dataToSeries(paramData)
+        };
+    }
+
+    return (
+        <EChartsReact 
+            style={ props.height !== undefined ? { height: props.height} : {}} 
+            option={parametersToEChartsOption(props.parameters)}
+            theme={'dark'}/>
+    )
+}
+
+export default ExerciseChart;
