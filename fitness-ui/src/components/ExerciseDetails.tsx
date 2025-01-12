7f832a10707fb00b7eee82478400d2488012367d
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-01-12 23:02:38 +0100

    Handle empty summary parameters for Exercises #126 (#136)

--- fitness-ui/src/components/ExerciseDetails.tsx
+++ fitness-ui/src/components/ExerciseDetails.tsx
@@ -41,7 +41,13 @@ const ExerciseDetails = () => {
                 from: from,
                 to: to 
             })
-            .then(s => s[0]);
+            .then(s => {
+                if (s.length === 0) {
+                    return { id: "", parameters: new Map() }
+                } else {
+                    return s[0];
+                }
+            });
         setData({exercise: e, summary: s});
     }, async () => {
         // NOOP
@@ -52,6 +58,7 @@ const ExerciseDetails = () => {
             <Box sx={{ display: "flex", paddingY: 2, paddingX: 1 }}>
                 <Typography variant="h6" color="white" sx={{ flexGrow: 1 }}>
                     { data.exercise.name }
+                    { data.summary.parameters.size === 0 ? " (No data available)" : "" }
                 </Typography>
             </Box>
             <Paper sx={{ height: height - 160 }}>
