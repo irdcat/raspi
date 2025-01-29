301f26b177f378c4ec7b52503cfc11ca54c08c5b
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-01-29 20:49:45 +0100

    Training Templates page #104 (#174)

---
+++ fitness-ui/src/api/TemplatesApi.ts
@@ -0,0 +1,45 @@
+import TrainingTemplate from "../model/TrainingTemplate";
+
+export default class TemplatesApi {
+
+    static async get(): Promise<Array<TrainingTemplate>> {
+        return fetch("/api/templates")
+            .then(response => response.json());
+    }
+
+    static async getById(id: string): Promise<TrainingTemplate> {
+        return fetch(`/api/templates/${id}`)
+            .then(response => response.json());
+    }
+
+    static async add(template: TrainingTemplate): Promise<TrainingTemplate> {
+        return fetch("/api/templates", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(template)
+        }).then(response => response.json());
+    }
+
+    static async update(id: string, template: TrainingTemplate): Promise<TrainingTemplate> {
+        return fetch(`/api/templates/${id}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(template)
+        }).then(response => response.json());
+    }
+
+    static async delete(id: string): Promise<TrainingTemplate> {
+        return fetch(`/api/templates/${id}`, {
+            method: "delete",
+            headers: {
+                "Accept": "application/json"
+            }
+        }).then(response => response.json());
+    }
+}
