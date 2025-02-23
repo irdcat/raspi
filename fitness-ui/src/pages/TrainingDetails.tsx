9d21cc92a352fed951a690bef711ea5154e7fcdc
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-02-24 00:29:25 +0100

    TrainingDetails page refinement and cleanup

--- fitness-ui/src/pages/TrainingDetails.tsx
+++ fitness-ui/src/pages/TrainingDetails.tsx
@@ -1,79 +1,101 @@
-import { Backdrop, Box, Button, CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { Backdrop, Box, Button, CircularProgress, IconButton, Paper, TextField, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2"
 import { format, parseISO } from "date-fns";
-import useRequiredParams from "../hooks/useRequiredParams";
 import { useEffect, useState } from "react";
 import { Training } from "../types";
 import { fetchTraining } from "../api/trainingApi";
 import ExerciseWithIcon from "../components/ExerciseWithIcon";
+import { LuPlus } from "react-icons/lu";
+import { useParams } from "react-router-dom";
 
 const TrainingDetails = () => {
-    const { dateString } = useRequiredParams(["dateString"])
-    const date = parseISO(dateString);
+    const { dateString } = useParams();
     const [loading, setLoading] = useState(true);
     const [training, setTraining] = useState<Training>({
-        date: date,
+        date: new Date(),
         bodyweight: 0,
         exercises: []
     });
 
     useEffect(() => {
         const fetchData = async () => {
+            if (dateString === undefined) {
+                return;
+            }
             setLoading(true);
+            const date = parseISO(dateString);
             const result = await fetchTraining(date);
             setTraining(result!!);
             setLoading(false);
         };
         fetchData();
-    }, []);
+    }, [dateString]);
 
     return (
         <>
             <Box sx={{ height: '100%', paddingX: '5px' }}>
                 <Box component={Paper} sx={{ height: '64px', padding: '16px' }}>
                     <Typography sx={{ fontSize: '24px' }}>
-                        {format(date, "dd.MM.yyyy")}
+                        {format(training.date, "dd.MM.yyyy")}
                     </Typography>
                 </Box>
                 <Box sx={{ height: 'calc(100% - 192px)', padding: '6px', overflowY: 'auto' }}>
-                    <Box sx={{ maxWidth: '768px', marginX: 'auto' }}>
-                    <Box sx={{ padding: '3px'}}>
-                        <TextField fullWidth label="Bodyweight (kg)"/>
+                    <Box sx={{ padding: '8px' }}>
+                        <Typography variant="h6">
+                            Details
+                        </Typography>
                     </Box>
-                    {training.exercises.map((trainingExercise, index) => (
-                        <Box key={index} sx={{ border: '1px solid rgba(255, 255, 255, 0.23)', borderRadius: '4px', margin: '8px 3px', padding: '3px' }}>
-                            <TableContainer>
-                                <Table size="small">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell colSpan={5}>
-                                                <ExerciseWithIcon exercise={trainingExercise.exercise}/>
-                                            </TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {trainingExercise.sets.map((set, setIndex) => (
-                                            <TableRow
-                                                key={setIndex}
-                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                                <TableCell width={5}>
-                                                    <Box sx={{ paddingLeft: '5px' }}>
-                                                        {setIndex + 1}
-                                                    </Box>
-                                                </TableCell>
-                                                <TableCell width={10}>
-                                                    {set.repetitions}
-                                                </TableCell>
-                                                <TableCell colSpan={3}>
-                                                    {set.weight} kg
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </Box>
-                    ))}
+                    <Box sx={{ padding: '8px' }}>
+                        <TextField 
+                            fullWidth
+                            size="small" 
+                            label="Bodyweight (kg)"
+                            value={training.bodyweight}/>
                     </Box>
+                    <Box sx={{ padding: '7px', display: 'flex' }}>
+                        <Typography sx={{ flexGrow: 1, alignSelf: 'center' }} variant="h6">
+                            Exercises
+                        </Typography>
+                        <IconButton
+                            tabIndex={-1} 
+                            color="success" 
+                            sx={{ border: '1px solid rgba(255, 255, 255, 0.23)', borderRadius: '4px' }}>
+                            <LuPlus/>
+                        </IconButton>
+                    </Box>
+                    <Grid container spacing={2} sx={{ padding: '5px' }}>
+                        {training.exercises.map((trainingExercise, index) => (
+                            <Grid 
+                                key={index} 
+                                sx={{ border: '1px solid rgba(255, 255, 255, 0.23)', borderRadius: '4px', padding: '3px' }}
+                                size={{ xs: 12, md: 6 }}>
+                                <Box sx={{ display: 'flex', padding: '5px', columnGap: '5px' }}>
+                                    <ExerciseWithIcon 
+                                        exercise={trainingExercise.exercise} 
+                                        sx={{ flexGrow: 1, alignSelf: 'center' }}/>
+                                    <IconButton tabIndex={-1} color="success">
+                                        <LuPlus/>
+                                    </IconButton>
+                                </Box>
+                                {trainingExercise.sets.map((set, setIndex) => (
+                                    <Box 
+                                        key={setIndex} 
+                                        sx={{ display: 'flex', borderTop: '1px solid rgba(255, 255, 255, 0.23)', columnGap: '10px', padding: '5px', alignItems: 'center' }}>
+                                        <Box>
+                                            {setIndex + 1}.
+                                        </Box>
+                                        <Box sx={{ flexGrow: 1 }}>
+                                            <TextField fullWidth size="small" value={set.repetitions}/>
+                                        </Box>
+                                        <Box>x</Box>
+                                        <Box sx={{ flexGrow: 1 }}>
+                                            <TextField fullWidth size="small" value={set.weight}/>
+                                        </Box>
+                                    </Box>
+                                ))}
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Box>
                 <Box component={Paper} sx={{ height: '64px' }}>
                     <Box sx={{ width: '100%', display: 'flex', padding: '14px', columnGap: '8px', flexDirection: 'row-reverse' }}>
