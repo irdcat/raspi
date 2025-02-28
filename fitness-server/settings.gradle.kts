db993e915266b6ea77c7d8246749c348b10a7870
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-02-28 23:42:39 +0100

    Tidy up dependencies and manage versions in properties (#267)

--- fitness-server/settings.gradle.kts
+++ fitness-server/settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "fitness-server"
+
+pluginManagement {
+    val jvmKotlinVersion: String by settings
+    val springKotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyMgmtVersion: String by settings
+    val koverVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version jvmKotlinVersion
+        kotlin("plugin.spring") version springKotlinVersion
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyMgmtVersion
+        id("org.jetbrains.kotlinx.kover") version koverVersion
+    }
+}
