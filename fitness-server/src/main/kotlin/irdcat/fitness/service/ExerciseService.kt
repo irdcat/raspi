6afaddb1e122328e2866da6ab69719a63cc7e080
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-06-01 02:09:56 +0200

    Correclt mark 1 rep max #425 (#426)

--- fitness-server/src/main/kotlin/irdcat/fitness/service/ExerciseService.kt
+++ fitness-server/src/main/kotlin/irdcat/fitness/service/ExerciseService.kt
@@ -4,9 +4,8 @@ import irdcat.fitness.service.dto.CountedExerciseDto
 import irdcat.fitness.service.model.TrainingExercise
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort.Direction
-import org.springframework.data.mongodb.MongoExpression
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.aggregation.AccumulatorOperators
+import org.springframework.data.mongodb.core.aggregation.AccumulatorOperators.Max
 import org.springframework.data.mongodb.core.aggregation.AddFieldsOperation.addField
 import org.springframework.data.mongodb.core.aggregation.Aggregation.group
 import org.springframework.data.mongodb.core.aggregation.Aggregation.limit
@@ -15,13 +14,13 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregat
 import org.springframework.data.mongodb.core.aggregation.Aggregation.project
 import org.springframework.data.mongodb.core.aggregation.Aggregation.skip
 import org.springframework.data.mongodb.core.aggregation.Aggregation.sort
-import org.springframework.data.mongodb.core.aggregation.AggregationExpression
-import org.springframework.data.mongodb.core.aggregation.AggregationOptions
 import org.springframework.data.mongodb.core.aggregation.ArrayOperators.Filter
 import org.springframework.data.mongodb.core.aggregation.ConditionalOperators.Cond.`when`
 import org.springframework.data.mongodb.core.aggregation.ArrayOperators.Size
 import org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Eq
 import org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Gt
+import org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Gte
+import org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Ne
 import org.springframework.data.mongodb.core.query.Criteria.where
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -38,11 +37,15 @@ class ExerciseService(
         const val SET = "set"
         const val SET_REPETITIONS = "set.repetitions"
         const val SINGLE_REP_SETS = "singleRepSets"
+        const val NON_SINGLE_REP_SETS = "nonSingleRepSets"
         const val SINGLE_REP_SETS_WEIGHT = "singleRepSets.weight"
+        const val NON_SINGLE_REP_SETS_WEIGHT = "nonSingleRepSets.weight"
         const val EXERCISE = "exercise"
         const val EXERCISE_NAME = "exercise.name"
         const val COUNT = "count"
+        const val MAYBE_BEST = "maybeBest"
         const val BEST = "best"
+        const val NON_BEST = "nonBest"
         const val GROUP_KEY = "_id"
     }
 
@@ -61,24 +64,46 @@ class ExerciseService(
         val singleRepSetsAddFieldOperation = addField(SINGLE_REP_SETS)
             .withValue(
                 Filter.filter(SETS)
-                .`as`(SET)
-                .by(Eq.valueOf("\$\$${SET_REPETITIONS}").equalToValue(1)))
+                    .`as`(SET)
+                    .by(Eq.valueOf("\$\$${SET_REPETITIONS}").equalToValue(1)))
             .build()
-        val bestAddFieldOperation = addField(BEST)
+        val nonSingleRepSetsAddFieldOperation = addField(NON_SINGLE_REP_SETS)
+            .withValue(
+                Filter.filter(SETS)
+                    .`as`(SET)
+                    .by(Ne.valueOf("\$\$${SET_REPETITIONS}").notEqualToValue(1)))
+            .build()
+        val addMaybeBestOperation = addField(MAYBE_BEST)
             .withValue(
                 `when`(Gt.valueOf(Size.lengthOfArray(SINGLE_REP_SETS)).greaterThanValue(0))
-                    .then(AccumulatorOperators.Max.maxOf(SINGLE_REP_SETS_WEIGHT))
+                    .then(Max.maxOf(SINGLE_REP_SETS_WEIGHT))
                     .otherwise(-1.0f))
             .build()
-        val excludeSingleRepSets = project()
-            .andExclude(SINGLE_REP_SETS)
+        val nonSingleRepsBestAddFieldOperation = addField(NON_BEST)
+            .withValue(
+                `when`(Gt.valueOf(Size.lengthOfArray(NON_SINGLE_REP_SETS)).greaterThanValue(0))
+                    .then(Max.maxOf(NON_SINGLE_REP_SETS_WEIGHT))
+                    .otherwise(-1.0f))
+            .build()
         val groupOperation = group(EXERCISE)
             .count().`as`(COUNT)
-            .max(BEST).`as`(BEST)
+            .max(MAYBE_BEST).`as`(MAYBE_BEST)
+            .max(NON_BEST).`as`(NON_BEST)
         val projectionOperation = project()
             .and(GROUP_KEY).`as`(EXERCISE)
-            .and(COUNT).`as`(COUNT)
-            .and(BEST).`as`(BEST)
+            .andInclude(COUNT)
+            .andInclude(MAYBE_BEST)
+            .andInclude(NON_BEST)
+        val addBestOperation = addField(BEST)
+            .withValue(
+                `when`(Gte.valueOf(NON_BEST).greaterThanEqualTo(MAYBE_BEST))
+                    .then(-1.0f)
+                    .otherwiseValueOf(MAYBE_BEST))
+            .build()
+        val projection2Operation = project()
+            .andInclude(EXERCISE)
+            .andInclude(COUNT)
+            .andInclude(BEST)
         val sortOperation = sort(Direction.ASC, EXERCISE_NAME)
         val skipOperation = skip((page * pageSize).toLong())
         val limitOperation = limit(pageSize.toLong())
@@ -88,10 +113,13 @@ class ExerciseService(
         val aggregation = newAggregation(
             matchOperation,
             singleRepSetsAddFieldOperation,
-            bestAddFieldOperation,
-            excludeSingleRepSets,
+            nonSingleRepSetsAddFieldOperation,
+            addMaybeBestOperation,
+            nonSingleRepsBestAddFieldOperation,
             groupOperation,
             projectionOperation,
+            addBestOperation,
+            projection2Operation,
             sortOperation,
             skipOperation,
             limitOperation)
@@ -102,6 +130,8 @@ class ExerciseService(
             countingGroupOperation)
 
         data class CountIntermediate(val count: Long)
+
+        logger.debug("Aggregation: {}", aggregation)
 
         val resultsMono = reactiveMongoTemplate
             .aggregate(aggregation, TrainingExercise::class.java, CountedExerciseDto::class.java)
