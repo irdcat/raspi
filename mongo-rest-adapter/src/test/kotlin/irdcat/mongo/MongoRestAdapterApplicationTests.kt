c61227c22dc6ac80048f446dc86b1afe5767de12
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-06-07 14:27:55 +0200

    WIP Bootstrap mongo-rest-adapter

---
+++ mongo-rest-adapter/src/test/kotlin/irdcat/mongo/MongoRestAdapterApplicationTests.kt
@@ -0,0 +1,13 @@
+package irdcat.mongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoRestAdapterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
