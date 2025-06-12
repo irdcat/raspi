b6baf0bb1749dfbdd93b58860e455a3dcbb48076
Kamil Adamiec <kamil.adamiec97@gmail.com>
2025-06-12 13:14:54 +0200

    Fix API GW (#496)

--- backend/api-gateway/src/main/kotlin/irdcat/apigw/ApiGatewayConfiguration.kt
+++ backend/api-gateway/src/main/kotlin/irdcat/apigw/ApiGatewayConfiguration.kt
@@ -1,5 +1,6 @@
 package irdcat.apigw
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.filters
@@ -11,10 +12,13 @@ import org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties(ApiGatewayProperties::class)
 class ApiGatewayConfiguration {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     @Bean
     fun routeLocator(rlb: RouteLocatorBuilder, props: ApiGatewayProperties) = rlb
         .routes {
             props.services.reversed().mapIndexed { index, item ->
+                log.info("Configuring route $index $item")
                 route(item.name) {
                     path("${item.prefix}/*").or().path(item.prefix)
                     filters {
